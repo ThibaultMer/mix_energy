6315531fe9c40a14051f83cb3ff5b4cef6540d22
Kuang Zheng <zhengk1988@gmail.com>
2026-04-06 16:24:01 +0000

    Enlèvement des champs non utilisés, ajout du modèle kpi

--- dbt/models/silver/eco2mix_national_cons_def_histo.sql
+++ dbt/models/silver/eco2mix_national_cons_def_histo.sql
@@ -4,7 +4,27 @@
     unique_key='date'
 ) }}
 
-SELECT * FROM {{source('nat_source','eco2mix_national_cons_def')}}
+SELECT 
+perimetre       AS perimetre,
+nature          AS nature,
+date            AS date,
+heure           AS heure,
+date_heure      AS date_heure,
+consommation    AS consommation,
+prevision_j1    AS prevision_j1,
+prevision_j     AS prevision_j,
+fioul           AS fioul,
+charbon         AS charbon,
+gaz             AS gaz,
+nucleaire       AS nucleaire,
+eolien          AS eolien,
+solaire         AS solaire,
+hydraulique     AS hydraulique,
+pompage         AS pompage,
+bioenergies     AS bioenergies,
+ech_physiques   AS ech_physiques,
+taux_co2        AS taux_co2
+FROM {{source('nat_source','eco2mix_national_cons_def')}}
 
 {% if is_incremental() %}
     WHERE date > (SELECT MAX(date) FROM {{ this }})
