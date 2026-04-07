494e5908a0eb7412963ab614060d71923f63b606
Xavier Filaire <xfilairepro@gmail.com>
2026-04-07 16:48:35 +0000

    Save work for prediction

--- predict/src/predict/data.py
+++ predict/src/predict/data.py
@@ -7,35 +7,24 @@ import google.oauth2.service_account as service_account
 
 from predict import get_logger
 
+from datetime import datetime, timedelta
+
 
 BIGQUERY_DATASET = os.getenv("BIGQUERY_DATASET", "TEST_DBT_GOLD")
 
 QUERIES = {
     "region": (
         "reg_tr_predi",
-        "code_insee_region,month,day,hour,minute,,consommation,prev_conso_mean,prev_conso_mean_h,prev_conso_mean_m ",
+        "code_insee_region,day,month,hour,minute,,consommation,prev_conso_mean,prev_conso_mean_h,prev_conso_mean_m ",
     ),
     "national": (
         "nat_tr_predi",
-        "month,day,hour,minute,consommation,prevision_j1,prevision_j",
+        "day,month,hour,minute,consommation,prevision_j1,prevision_j",
     ),
 }
 
 
-def load_data(is_national: bool = True) -> pd.DataFrame:
-    """
-    Loads data from the Gold table stored in BIGQUERY_DATASET
-
-    Params
-    ------
-    is_national : Retrieve data from the National ? (default True). Otherwise, it will use the by Region dataset
-
-    Return
-    ------
-    A DataFrame loaded with the data from the specified dataset
-    None if an error occured
-    """
-
+def connect_to_bigquery() -> bigquery.Client | None:
     # Create the credential used to authenticate
 
     json_credential_file = os.getenv(
@@ -64,15 +53,45 @@ def load_data(is_national: bool = True) -> pd.DataFrame:
         get_logger().error("Fail to connect to project {} : {}".format(project_id, e))
         return None
 
+    return client
+
+
+def load_data(
+    client: bigquery.Client, is_national: bool = True, is_all: bool = True
+) -> pd.DataFrame:
+    """
+    Loads data from the Gold table stored in BIGQUERY_DATASET
+
+    Params
+    ------
+    is_national : Retrieve data from the National ? (default True). Otherwise, it will use the by Region dataset
+
+    Return
+    ------
+    A DataFrame loaded with the data from the specified dataset
+    None if an error occured
+    """
+
     if is_national:
         query_data = QUERIES["national"]
     else:
         query_data = QUERIES["region"]
 
-    query = (
-        f"select {query_data[1]} FROM {BIGQUERY_DATASET}.{query_data[0]}"
-        " WHERE consommation is not NULL ORDER BY day ASC, month ASC, hour ASC, minute ASC"
-    )
+    if is_all:
+        query = (
+            f"select {query_data[1]} FROM {BIGQUERY_DATASET}.{query_data[0]}"
+            " WHERE consommation is not NULL ORDER BY date_heure ASC"
+        )
+    else:
+        current_dt = datetime.now() - timedelta(days=30)
+
+        compare_dt = current_dt.strftime("%Y-%m-%d 00:00:00")
+
+        query = (
+            f"select {query_data[1]},date_heure, COUNT(1) as total_count FROM {BIGQUERY_DATASET}.{query_data[0]} "
+            f"WHERE consommation is not NULL AND date_heure>='{compare_dt}' "
+            "ORDER BY date_heure DESC"
+        )
 
     try:
         df = client.query_and_wait(query).to_dataframe()
@@ -84,6 +103,33 @@ def load_data(is_national: bool = True) -> pd.DataFrame:
         return None
 
     return df
+
+
+# def build_input_national(df: pd.DataFrame):
+#     index = df["date_heure"].idxmax()
+
+#     df.iloc(index)
+
+#     current_dt = datetime.now()
+
+#     # The prediction is done for the next 15 minutes
+
+#     if current_dt.minute > 45:
+#         # If it is 15:47, the prediction will be done for 16:00
+#         # If it is 23:47, the prediction will be done the day following at 00:00
+#         current_dt = current_dt + timedelta(hours=1)
+#         minute = current_dt.minute
+#     else:
+#         # If it is 14:13, we will perform a prediction for 14:15
+#         minute = int(current_dt.minute / 15) * 15 + 15
+
+#     values = [current_dt.day, current_dt.month, current_dt.hour, minute, 0, 0, 0]
+
+#     input_values = dict(zip(QUERIES["national"][1], values))
+
+
+# def build_input_region(df: pd.DataFrame, code_insee: int):
+#     index = df["date_heure"].idxmax()
 
 
 def create_X_y(
--- predict/src/predict/train.py
+++ predict/src/predict/train.py
@@ -1,6 +1,6 @@
 from predict.model import Energypredict
 from predict.mllogs import MlFlowLogger
-from predict.data import load_data, create_X_y
+from predict.data import connect_to_bigquery, load_data, create_X_y
 
 
 def train(
@@ -8,7 +8,15 @@ def train(
     test_size: float = 0.2,
     random_state: int = 42,
 ) -> None:
-    """ """
+    """
+    Train a model
+
+    Params
+    ------
+    is_national : Do we train for National values ? or Region values ?
+    test_size: used to split the dataset between the values to use for training and the ones for testing
+    random_state: used for the dataset split function
+    """
 
     if is_national:
         savename = "national_model"
@@ -19,7 +27,9 @@ def train(
         predicteng = Energypredict(mlogger)
 
         # Import the dataset
-        df = load_data(is_national)
+        bqclient = connect_to_bigquery()
+        if bqclient is not None:
+            df = load_data(bqclient, is_national)
 
         if df is not None:
             # Split the dataset and apply necessary transformation to prepare data for the training
@@ -27,17 +37,36 @@ def train(
                 df, test_size=test_size, random_state=random_state
             )
 
+            # Prepare data for the training
             X_train_preproc = predicteng.preprocess_data(X_train, train=True)
             X_test_preproc = predicteng.preprocess_data(X_test, train=False)
 
             predicteng.create_model()
 
+            # Train the model
             predicteng.train_model(X_train=X_train_preproc, y_train=y_train)
+
+            # Evaluate the model
             predicteng.evaluate_model(X_test=X_test_preproc, y_test=y_test)
 
 
+# def predict_conso(bqclient, is_national: bool = True) -> float | None:
+#     if is_national:
+#         savename = "national_model"
+#     else:
+#         savename = "region_model"
+
+#     with MlFlowLogger(savename) as mlogger:
+#         predicteng = Energypredict(mlogger)
+
+#         df = load_data(bqclient, is_national, False)
+
+#         if df is None:
+#             return None
+
+
 if __name__ == "__main__":
-    # train(True)
+    train(True)
     train(False)
 
     # with MlFlowLogger("national_model") as mlogger:
